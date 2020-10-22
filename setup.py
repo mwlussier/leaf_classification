e0393506a2d86b7ef71fdb0cf7ae9881d249f286
mwlussier <mathieu.lussier@usherbrooke.ca>
2020-10-22 10:54:29 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Kaggle "Leaf Classification" database classification project for IFT712.',
+    author='Mathieu Lussier',
+    license='MIT',
+)
