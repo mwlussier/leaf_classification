35b6e0d8116f49f815fc0bedb2ab83287a183fd7
mwlussier <mathieu.lussier@usherbrooke.ca>
2020-11-03 17:31:23 -0500

    vanilla classifier initiate - will be used as a template to perform training and prediction of different models

---
+++ models/vanilla_classifier.py
@@ -0,0 +1,26 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.join(os.getcwd())))
+
+class VanillaClassifier:
+    """
+
+
+    """
+
+
+    def __inti__(self, model):
+        self.model = model
+
+
+    def training(self):
+        pass
+
+    def prediction(self):
+        pass
+
+    def evaluate(self):
+        pass
+
+    def pipeline(self):
+        pass
