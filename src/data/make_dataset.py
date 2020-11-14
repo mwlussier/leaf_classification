7e8ad882f7e7b38408c016ddbcb9c240317e33a1
mwlussier <mathieu.lussier@usherbrooke.ca>
2020-11-14 09:52:46 -0500

    src - process pipeline initiate

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,7 +3,7 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
-
+import pandas as pd
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
@@ -15,9 +15,19 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
+    ### RAW DATA PULL ###
+    train_data = pd.read_csv(input_filepath + '/train.csv')
+    test_data = pd.read_csv(input_filepath + '/test.csv')
+    sample_submission = pd.read_csv(input_filepath + '/sample_submission.csv')
 
-def testprint():
-    print("HELLO, WORLD")
+    ### PROCESSING ###
+
+
+    ### SAVED TO OUTPUT FILEPATH ###
+    train_data.to_csv(output_filepath + '/train_processed.csv')
+    test_data.to_csv(output_filepath + '/test_processed.csv')
+    sample_submission.to_csv(output_filepath + '/sample_submission_processed.csv')
+
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -25,7 +35,7 @@ if __name__ == '__main__':
 
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
-
+    print(project_dir)
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
